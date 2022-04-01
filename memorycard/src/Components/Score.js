b59fd7f3acb48427ca65746a74a8bcf114403ced
fadcny <fadcny@gmail.com>
2022-04-01 16:34:25 -0400

    added score history

--- memorycard/src/Components/Score.js
+++ memorycard/src/Components/Score.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import '../Styles/Score.css'
 
-function Score() {
+function Score(props) {
+  const {currentScore, scoreHistory} = props;
+  const scoreHistoryClone = scoreHistory.map((e) => e);
+  scoreHistoryClone.sort((a, b) => b - a);
+  const highScore = scoreHistoryClone[0];
+
   return (
     <div className='score-container'>
-      <div className="current-score">Current Score</div>
-      <div className="high-score">High Score</div>
+      <div className="current-score">{`Current Score: ${currentScore}`}</div>
+      <div className="high-score">{`High Score: ${highScore}`}</div>
     </div>
   )
 }
