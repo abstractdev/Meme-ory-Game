8607be7b31b052e34c2bb360811bd232abc7e480
fadcny <fadcny@gmail.com>
2022-04-01 16:34:40 -0400

    added card data

--- memorycard/src/Data/CardData.js
+++ memorycard/src/Data/CardData.js
@@ -1,12 +1,15 @@
-export const card0 = { image: null,text: '', cardId : 0, randomId: null }
-export const card1 = { image: null,text: '', cardId : 1, randomId: null }
-export const card2 = { image: null,text: '', cardId : 2, randomId: null }
-export const card3 = { image: null,text: '', cardId : 3, randomId: null }
-export const card4 = { image: null,text: '', cardId : 4, randomId: null }
-export const card5 = { image: null,text: '', cardId : 5, randomId: null }
-export const card6 = { image: null,text: '', cardId : 6, randomId: null }
-export const card7 = { image: null,text: '', cardId : 7, randomId: null }
-export const card8 = { image: null,text: '', cardId : 8, randomId: null }
+const card0 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/030/157/womanyellingcat.jpg",text: "Woman Yelling At Cat", cardId : 0, randomId: null }
+const card1 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/021/985/image.jpg",text: "Cash Me Ousside", cardId : 1, randomId: null }
+const card2 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/023/732/damngina.jpg",text: "Distracted Boyfriend", cardId : 2, randomId: null }
+const card3 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/018/012/this_is_fine.jpg",text: "This Is Fine", cardId : 3, randomId: null }
+const card4 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/006/236/1stWorld.jpg",text: "First World Problems", cardId : 4, randomId: null }
+const card5 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/006/026/NOTSUREIF.jpg",text: "Not Sure If", cardId : 5, randomId: null }
+const card6 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/008/320/condecendingwonka.jpg",text: "Condescending Wonka", cardId : 6, randomId: null }
+const card7 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg",text: "Doge", cardId : 7, randomId: null }
+const card8 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/007/508/neildegrasse.jpg",text: "Neil Degrasse Tyson", cardId : 8, randomId: null }
+const card9 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/029/959/Screen_Shot_2019-06-05_at_1.26.32_PM.jpg",text: "Stonks", cardId : 9, randomId: null }
+const card10 = { image: "https://i.kym-cdn.com/photos/images/newsfeed/001/780/944/516.jpg",text: "Bro Explaining", cardId : 10, randomId: null }
+const card11 = { image: "https://i.kym-cdn.com/entries/icons/mobile/000/017/299/DmbzJspWwAEprcQ.jpg",text: "Is This A Pigeon?", cardId : 11, randomId: null }
 export const cardArray = [
   card0,
   card1,
@@ -17,4 +20,7 @@ export const cardArray = [
   card6,
   card7,
   card8,
+  card9,
+  card10,
+  card11
 ]
