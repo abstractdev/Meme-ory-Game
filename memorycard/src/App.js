6d993fd695f18bd815c9ff04b119e4f127f6760c
fadcny <fadcny@gmail.com>
2022-04-01 16:33:28 -0400

    game logic

--- memorycard/src/App.js
+++ memorycard/src/App.js
@@ -6,17 +6,17 @@ import { cardArray } from './Data/CardData';
 import './App.css';
 
 function App() {
-  const [cards, setCards] = useState(cardArray);
+  const [cards, setCards] = useState([]);
   const [clickedCards, setClickedCards] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+  const [scoreHistory, setScoreHistory] = useState([0]);
 
   const shuffleCards = () => {
-    //get a Set of 9 numbers in randomized order
+    //get a Set of numbers in randomized order
     const cardsClone = cardArray.map((e) => e)
     const set = new Set ();
-    while (set.size < 9) {
-      set.add(Math.floor(Math.random() * 9))
+    while (set.size < cardArray.length) {
+      set.add(Math.floor(Math.random() * cardArray.length))
     }
     //give each card a random unique identifier
     const setClone = [];
@@ -33,26 +33,38 @@ function App() {
 
   useEffect(() => {
     shuffleCards();
-    console.log(cards);
   }, [])
 
   const handleClick = (event) => {
-    console.log(event.target.dataset.id)
-    // console.log(shuffleCards());
-    console.log(cards)
-    // switch (event.target.dataset.id) {
-    //   case 'card0':
-    //     setClickedCards([...clickedCards, ])
-    
-    //   default:
-    //     break;
+    if (event.target.dataset.id) {
+    //match dataset id and card id and add to clicked state
+    const clickedCard = cardArray.filter((e) => {
+       return e.cardId === parseInt(event.target.dataset.id)
+      });
+      //check if card is a duplicate
+      //if card is a duplicate set score history
+    if (clickedCards.some((e => e.cardId === clickedCard[0].cardId))) {
+      // const finalScore = [new Set(clickedCards).size]
+      setScoreHistory([...scoreHistory, currentScore]);
+      setCurrentScore(0);
+      setClickedCards([]);
+      alert('Duplicate')
+    } else {
+      // set clicked cards state
+      setClickedCards([...clickedCards, ...clickedCard])
+      //if not a duplicate, shuffle cards and continue game
+      shuffleCards()
+      //increment current score
+      setCurrentScore(currentScore + 1)
     }
+  }
+  }
 
   return (
     <div className="App">
       <Header />
-      <Score />
-        <Card handleClick = {handleClick} cards = {cards}/>
+      <Score currentScore={currentScore} scoreHistory={scoreHistory}/>
+        <Card handleClick={handleClick} cards={cards}/>
     </div>
   );
 }
