41e2f26b40d23b8645069bd3459416b92f47aa24
abstractdev <fadcny@gmail.com>
2022-05-10 23:45:49 -0400

    added github link

--- memorycard/src/Components/Header.js
+++ memorycard/src/Components/Header.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "../Styles/Header.css";
+import github from "../GitHub-Mark-32px.png";
 
 function Header() {
-  return <div className="header">Meme-ory Game</div>;
+  return (
+    <>
+      <div className="header">Meme-ory Game</div>
+      <a
+        className="github-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/abstractdev"
+      >
+        <img className="github-logo" src={github} alt="github link" />
+        abstractdev
+      </a>
+    </>
+  );
 }
 
 export default Header;
