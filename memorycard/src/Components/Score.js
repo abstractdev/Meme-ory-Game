a6b1d02bc3f6869ba4900bc4568497b57953f36b
abstractdev <fadcny@gmail.com>
2022-05-13 01:17:33 -0400

    added github link

--- memorycard/src/Components/Score.js
+++ memorycard/src/Components/Score.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Styles/Score.css";
+import github from "../GitHub-Mark-Light-32px.png";
 
 function Score({ currentScore, scoreHistory }) {
   const scoreHistoryClone = scoreHistory.map((e) => e);
@@ -9,6 +10,15 @@ function Score({ currentScore, scoreHistory }) {
   return (
     <div className="score-container">
       <div className="current-score">{`Current Score: ${currentScore}`}</div>
+      <a
+        className="github-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/abstractdev"
+      >
+        <img className="github-logo" src={github} alt="github link" />
+        abstractdev
+      </a>
       <div className="high-score">{`High Score: ${highScore}`}</div>
     </div>
   );
